f03a65af3b40b9faae032b898c232b62322c1a8b
Connor Imes <cimes@isi.edu>
2022-02-22 10:40:24 -0500

    xdc: msr-safe: add -q parameter to apt-get

--- xdc/host-install-msr-safe.sh
+++ xdc/host-install-msr-safe.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
-sudo apt-get install -y "linux-headers-$(uname -r)" || exit $?
+sudo apt-get install -q -y "linux-headers-$(uname -r)" || exit $?
 git clone https://github.com/LLNL/msr-safe.git || exit $?
 cd msr-safe || exit $?
 git checkout v1.6.0 || exit $?
