993688f16f06e50df27002a1262e041847f05562
Connor Imes <cimes@isi.edu>
2022-02-22 10:39:51 -0500

    xdc: packages: need dist-upgrade to get kernel updates now

--- xdc/host-install-packages.sh
+++ xdc/host-install-packages.sh
@@ -10,3 +10,6 @@ sudo apt-mark hold grub2 grub-efi || exit $?
 sudo apt-get --allow-releaseinfo-change update || exit $?
 sudo apt-get -q -y upgrade || exit $?
 sudo apt-get -q -y install build-essential cmake git pkg-config || exit $?
+
+# Seems to be necessary to get kernel update (maybe since we're blocking grub)
+sudo apt-get -q -y dist-upgrade || exit $?
